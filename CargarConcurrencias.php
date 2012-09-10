da6b02a7aed054b0ef6300087152dfbd903ac257
st4nn <jhonathan2@gmail.com>
2012-09-10 13:05:01 -0500

    Primera Versión Funcional

---
+++ CargarConcurrencias.php
@@ -0,0 +1,73 @@
+<?php 
+   include("conectar.php"); 
+
+	$Fecha1 = $_POST['Fecha1'];
+	$Fecha2 = $_POST['Fecha2'];
+	
+	if ($Agrupacion == "Hora")
+	{
+		$Campo = "Hora";
+		$Funcion = "HOUR";
+		$Label = "CONCAT( HOUR( Hora ) ,  ':00' )";
+	}
+	elseif ($Agrupacion =="Dia")
+	{
+		$Campo = "Fecha";
+		$Funcion = "DAY";
+		$Label = "Fecha";
+	}
+	elseif ($Agrupacion =="Mes")
+	{
+		$Campo = "Fecha";
+		$Funcion = "MONTH";
+		$Label = "MONTH(Fecha)";
+	}
+	
+	$link=Conectarse();
+	$sql = "
+	SELECT 
+		Ip,
+		Fecha,
+		Hora,
+		Conexiones,
+		IO,
+		URL
+	FROM
+		Estadisticas1
+	WHERE 
+		Fecha BETWEEN '$Fecha1' AND '$Fecha2'
+	";
+	
+	$result=mysql_query($sql,$link); 
+
+$row = mysql_fetch_array($result);
+
+		class Connections
+	{
+		public $Fecha;
+		public $Hora;
+		public $Ip;
+		public $Conexiones;
+		public $IO;
+		public $URL;
+	}
+	$Index = 0;
+	
+	do 
+	{ 
+		$Connection[$Index] = new Connections();
+		
+		$Connection[$Index]->Fecha = $row['Fecha'];
+		$Connection[$Index]->Hora = $row['Hora'];
+		$Connection[$Index]->Ip = $row['Ip'];
+		$Connection[$Index]->Conexiones = $row['Conexiones'];
+		$Connection[$Index]->IO = $row['IO'];
+		$Connection[$Index]->URL = $row['URL'];
+
+		$Index++;
+	} while($row = mysql_fetch_array($result));
+
+	mysql_close($link);	
+	
+	echo json_encode($Connection);
+?>
---
+++ CargarConexiones.php
@@ -0,0 +1,65 @@
+<?php 
+   include("conectar.php"); 
+	$Agrupacion = $_POST['Agrupacion'];
+	$Fecha1 = $_POST['Fecha1'];
+	$Fecha2 = $_POST['Fecha2'];
+	
+	if ($Agrupacion == "Hora")
+	{
+		$Campo = "Hora";
+		$Funcion = "HOUR";
+		$Label = "CONCAT( HOUR( Hora ) ,  ':00' )";
+	}
+	elseif ($Agrupacion =="Dia")
+	{
+		$Campo = "Fecha";
+		$Funcion = "DAY";
+		$Label = "Fecha";
+	}
+	elseif ($Agrupacion =="Mes")
+	{
+		$Campo = "Fecha";
+		$Funcion = "MONTH";
+		$Label = "MONTH(Fecha)";
+	}
+	
+	$link=Conectarse();
+	$sql = "
+	SELECT 
+		$Funcion($Campo) AS 'Fecha', 
+		COUNT( Conexiones ) AS 'Conexiones',
+		$Label AS FechaII
+	FROM Estadisticas1
+	WHERE 
+		IO = 'in' AND (Fecha BETWEEN '$Fecha1' AND '$Fecha2') 
+	GROUP BY $Funcion($Campo) 
+	";
+	
+	$result=mysql_query($sql,$link); 
+
+$row = mysql_fetch_array($result);
+
+		class Connections
+	{
+		public $Fecha;
+		public $Conexiones;
+		public $FechaII;
+	}
+	$Index = 0;
+
+	do 
+	{ 
+		$Connection[$Index] = new Connections();
+		
+		$Connection[$Index]->Fecha = $row['Fecha'];
+		$Connection[$Index]->Conexiones = $row['Conexiones'];
+		$Connection[$Index]->FechaII = $row['FechaII'];
+
+		$Index++;
+	} while($row = mysql_fetch_array($result));
+
+	mysql_close($link);	
+	echo json_encode($Connection);	
+?> 
+
+
